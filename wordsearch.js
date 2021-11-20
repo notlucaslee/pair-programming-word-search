d8039a97c6c84edd445bdfd99bba9812c2d7bc7a
notlucaslee <lee.lucas.hyunguk@gmail.com>
2021-11-20 08:05:19 +0000

    word search assignment + David

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,38 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (!letters) return false;
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const horizontalBackJoin = letters.map(ls => ls.reverse().join(''));
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  const verticalBackJoin = transpose(letters).map(ls => ls.reverse().join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (const l of horizontalBackJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (const l of verticalBackJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  let result = [];
+  let innerArray = [];
+  let counter = 0;
+  while (counter < matrix[0].length) {
+    for (let item of matrix) {
+      innerArray.push(item[counter]);
+    }
+    result.push(innerArray);
+    innerArray = [];
+    counter++;
+  }
+  return result;
+};
+
+  
+module.exports = wordSearch;
